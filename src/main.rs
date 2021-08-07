8fb6830ce3bc0207be41ca615190ee40764a998f
techmccat <alessioa02004@gmail.com>
2021-08-07 23:28:00 +0200

    feat: add binary to encode data

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::{error::Error, fs::File, io::{self, BufReader, BufWriter, Read, Write, stdin, stdout}};
+
+use argh::FromArgs;
+use linear_quadtree::enc::{LinearQuadTree, video::VideoEncoder};
+
+#[derive(FromArgs)]
+/// Encode one or more frames using linear quadtrees
+struct Encode {
+    #[argh(subcommand)]
+    subs: SubCommands
+}
+
+#[derive(FromArgs)]
+#[argh(subcommand)]
+enum SubCommands {
+    Frame(Frame),
+    Sequence(Sequence),
+}
+
+#[derive(FromArgs)]
+#[argh(subcommand, name = "frame")]
+/// Compress a single frame
+struct Frame {
+    #[argh(option, short = 'i', default = "String::from(\"-\")")]
+    /// input file, defaults to standard input
+    input: String,
+    #[argh(option, short = 'o', default = "String::from(\"-\")")]
+    /// output file, defaults to standard output
+    output: String,
+}
+
+#[derive(FromArgs)]
+#[argh(subcommand, name = "sequence")]
+/// Compress multiple contiguous frames
+struct Sequence {
+    #[argh(option, short = 'i', default = "String::from(\"-\")")]
+    /// input file, defaults to standard input
+    input: String,
+    #[argh(option, short = 'o', default = "String::from(\"-\")")]
+    /// output file, defaults to standard output
+    output: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let args: Encode = argh::from_env();
+
+    match args.subs {
+        SubCommands::Frame(s) => frame(s),
+        SubCommands::Sequence(s) => sequence(s),
+    }.map_err(|e| e.into())
+}
+
+fn match_input(i: String) -> Box<dyn Read> {
+    match i.as_str() {
+        // TODO: check if stdio is a tty
+        "-" => Box::new(BufReader::new(stdin())),
+        _ => Box::new(BufReader::new(File::open(i).unwrap()))
+    }
+}
+
+fn match_output(i: String) -> Box<dyn Write> {
+    match i.as_str() {
+        // TODO: check if stdio is a tty
+        "-" => Box::new(BufWriter::new(stdout())),
+        _ => Box::new(BufWriter::new(File::create(i).unwrap()))
+    }
+}
+
+fn frame(args: Frame) -> io::Result<()> {
+    let mut input = match_input(args.input);
+    let output = match_output(args.output);
+
+    let mut buf = [0; 1024];
+    input.read_exact(&mut buf)?;
+
+    let mut enc = LinearQuadTree::new(output);
+    enc.parse_slice_12864(&buf)?;
+
+    Ok(())
+}
+
+fn sequence(args: Sequence)  -> io::Result<()> {
+    let mut input = match_input(args.input);
+    let output = match_output(args.output);
+
+    let mut enc = VideoEncoder::new(output);
+    io::copy(&mut input, &mut enc)?;
+
+    Ok(())
+}
